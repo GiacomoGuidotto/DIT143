b3201d7fabd56e67d6af1f513df47b0aed611180
Giacomo Guidotto <giacomo@Giacomos-MacBook-Pro.local>
2024-11-05 15:31:42 +0100

    feat: real project init

--- playground.hs
+++
@@ -1,8 +0,0 @@
-sort [] = []
-sort (x : xs) = sort lo ++ [x] ++ sort hi
-  where
-    lo = [y | y <- xs, y < x]
-    hi = [y | y <- xs, y >= x]
-
-main = do
-  print $ sort [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
