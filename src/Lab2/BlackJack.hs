087eab5500cd7beb1ccd5dd7ed2abc6239408b49
Giacomo Guidotto <giacomo@Giacomos-MacBook-Pro.local>
2024-11-08 08:17:16 +0100

    feat(lab2): display and value

--- src/Lab2/BlackJack.hs
+++ src/Lab2/BlackJack.hs
@@ -1,4 +1,4 @@
-module Lab2.BlackJack where
+module Lab2.BlackJack (hand2, sizeSteps, display, value) where
 
 import Lab2.Cards
 import Lab2.RunGame
@@ -20,3 +20,51 @@ sizeSteps =
     1 + 1 + 0,
     2
   ]
+
+-- | Displaying function ----------------------------------------------
+
+-- | Display the rank of a card, removing the constructor
+displayRank :: Rank -> String
+displayRank (Numeric n) = show n
+displayRank r = show r
+
+-- | Display the suit of a card, using unicode symbols
+displaySuit :: Suit -> String
+displaySuit Hearts = "♥"
+displaySuit Spades = "♠"
+displaySuit Diamonds = "♦"
+displaySuit Clubs = "♣"
+
+-- | Display a card
+displayCard :: Card -> String
+displayCard (Card r s) = displayRank r ++ " of " ++ displaySuit s
+
+-- | Display a hand
+display :: Hand -> String
+display Empty = ""
+display (Add c h) = displayCard c ++ "\n" ++ display h
+
+-- | Value function --------------------------------------------------
+
+-- | Compute the value of a rank with aces counting for 1
+valueRank :: Rank -> Integer
+valueRank (Numeric n) = n
+valueRank Ace = 1
+valueRank _ = 10
+
+-- | Compute the value of a hand and count the number of aces
+initalValue :: Hand -> (Integer, Integer)
+initalValue Empty = (0, 0)
+initalValue (Add c h) = (valueRank (rank c) + v, ace + a)
+  where
+    (v, a) = initalValue h
+    ace = if rank c == Ace then 1 else 0
+
+-- | Compute the value of a hand and adjust the value if the hand
+-- contains aces
+value :: Hand -> Integer
+value h =
+  let extra = min a ((21 - v) `div` 10)
+   in v + 10 * extra
+  where
+    (v, a) = initalValue h
--- test/Lab2Test.hs
+++ test/Lab2Test.hs
@@ -1,6 +1,26 @@
 module Main (main) where
 
 import Lab2.BlackJack
+import Lab2.Cards
+
+hand1 :: Hand
+hand1 =
+  Add
+    (Card Ace Hearts)
+    (Add (Card Ace Spades) Empty)
+
+testHand :: Hand -> IO ()
+testHand h = do
+  putStr (display h)
+  putStr " value: "
+  print (value h)
+  putStr "\n"
 
 main :: IO ()
-main = print sizeSteps
+main = do
+  putStr "sizeSteps:\n"
+  print sizeSteps
+  putStr "hand1:\n"
+  testHand hand1
+  putStr "hand2:\n"
+  testHand hand2
