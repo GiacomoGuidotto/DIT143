a87a3eac1359014f537aafed137f9526e93510f5
Giacomo Guidotto <giacomo@Giacomos-MacBook-Pro.local>
2024-11-30 10:54:45 +0100

    chore: add exe

--- src/Lab4/Expr.hs
+++ src/Lab4/Expr.hs
@@ -1,1 +1,50 @@
 module Expr where
+
+import Test.QuickCheck
+
+data Expr
+  = Num Double
+  | Var
+  | Op (Double -> Double -> Double) Expr Expr
+  | Function (Double -> Double) Expr
+
+instance Arbitrary Expr where
+  arbitrary = undefined
+
+x :: Expr
+x = undefined
+
+num :: Double -> Expr
+num = undefined
+
+add, mul :: Expr -> Expr -> Expr
+add = undefined
+mul = undefined
+
+sin, cos :: Expr -> Expr
+sin = undefined
+cos = undefined
+
+size :: Expr -> Int
+size = undefined
+
+showExpr :: Expr -> String
+showExpr = undefined
+
+eval :: Expr -> Double -> Double
+eval = undefined
+
+readExpr :: String -> Maybe Expr
+readExpr = undefined
+
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr = undefined
+
+arbExpr :: Int -> Gen Expr
+arbExpr = undefined
+
+simplify :: Expr -> Expr
+simplify = undefined
+
+differentiate :: Expr -> Expr
+differentiate = undefined
---
+++ src/Lab4/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified Calculator as Cmain
+
+main :: IO ()
+main = Cmain.main
