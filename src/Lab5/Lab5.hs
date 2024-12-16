3b1b0830339f3a4b23428c11465487eae3576829
Giacomo Guidotto <giacomo@Giacomos-MacBook-Pro.local>
2024-12-16 15:24:53 +0100

    feat(lab5): comments

--- src/Lab5/Lab5.hs
+++ src/Lab5/Lab5.hs
@@ -1,4 +1,14 @@
-module Lab5 where
+module Lab5
+  ( QA (Person, Question),
+    defaultTree,
+    readQAFromFile,
+    isYes,
+    askQuestions,
+    learnNew,
+    play,
+    main,
+  )
+where
 
 import Data.Char (toLower)
 import System.IO
@@ -25,6 +35,9 @@ defaultTree =
         (Person "Hillary Clinton")
     )
 
+-- initialize the QA tree by reading from a file and parsing the content
+-- fallback to the default tree if the file is not found or the content is
+-- not parsable
 readQAFromFile :: FilePath -> IO QA
 readQAFromFile fname = do
   res <- tryIOError (readFile fname)
@@ -32,26 +45,35 @@ readQAFromFile fname = do
     Left _ -> return defaultTree
     Right contents ->
       case readEither contents of
-        Left _ -> return defaultTree
+        Left _ -> do
+          putStrLn "Using the default quiz data."
+          return defaultTree
         Right qa -> return qa
 
+-- utility function to check if the user's input starts with 'y'
+-- to determine if the answer is yes (case-insensitive)
 isYes :: String -> Bool
 isYes ans = case map toLower ans of
   ('y' : _) -> True
   _ -> False
 
+-- singe match loop
+-- traverse the tree top-down to ask questions, rebuild the tree bottom-up
+-- to change the leaf node in case the computer's guess is wrong
 askQuestions :: QA -> IO QA
+-- leaf node: computer's guess
 askQuestions (Person name) = do
   putStr ("My guess: Is it " ++ name ++ "? ")
   hFlush stdout
   ans <- getLine
   if isYes ans
     then do
-      putStrLn "I won!"
+      putStrLn "Hurray! I won!"
       return (Person name)
     else do
       putStrLn "OK - you won this time."
       learnNew (Person name)
+-- internal node: ask a question
 askQuestions (Question q yesTree noTree) = do
   putStr (q ++ " ")
   hFlush stdout
@@ -64,6 +86,7 @@ askQuestions (Question q yesTree noTree) = do
       newNo <- askQuestions noTree
       return (Question q yesTree newNo)
 
+-- build a new node by asking the user the question
 learnNew :: QA -> IO QA
 learnNew (Person oldName) = do
   putStr "Just curious: Who was your famous person? "
@@ -73,22 +96,27 @@ learnNew (Person oldName) = do
   putStrLn $ "and the answer for \"" ++ oldName ++ "\" is \"no\"."
   hFlush stdout
   newQ <- getLine
-  let newNode = Question newQ (Person newPerson) (Person oldName)
-  return newNode
+  return (Question newQ (Person newPerson) (Person oldName))
 learnNew _ = error "learnNew called on a Question node, which should not happen."
 
+-- game loop
+-- generate a new tree after each match, uses it to play again or returns it
 play :: QA -> IO QA
 play tree = do
   newTree <- askQuestions tree
-  putStr "Play again? (yes/no): "
+  putStr "Play again? "
   hFlush stdout
   ans <- getLine
   if isYes ans
     then play newTree
     else return newTree
 
+-- parse tree from file, mutate it during the game, and write it back to the
+-- file
 main :: IO ()
 main = do
   tree <- readQAFromFile "questions.qa"
   finalTree <- play tree
+  putStrLn "Saving QA file..."
   writeFile "questions.qa" (show finalTree)
+  putStrLn "Bye!"
